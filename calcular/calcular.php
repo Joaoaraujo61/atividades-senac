2ea0015217654e6d0a7d04524cf42d698a4b0472
João Pedro de Araújo <79383821+Joaoaraujo61@users.noreply.github.com>
2026-06-25 16:52:27 -0300

    Delete calcular directory

--- calcular/calcular.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$numero1 = $_POST["primeiroNumero"];
-$numero2 = $_POST["segundoNumero"];
-$multiplicacao = $numero1*$numero2;
-
-echo("A multiplicação entre $numero1 e $numero2 é $multiplicacao")
-
-?>
