12dc47f37c0ca954b6c0e5643cd490bb86ce439a
João Pedro de Araújo <79383821+Joaoaraujo61@users.noreply.github.com>
2026-06-30 17:04:02 -0300

    Add files via upload

---
+++ atv19/index.php
@@ -0,0 +1,20 @@
+<?php
+
+include "conexao.php";
+
+$num1 = 2;
+$num2 = 52;
+$num3= 3;
+$res = $num2-$num1-$num3;
+
+$sql = "INSERT INTO calculo(numero1, numero2, numero3, resultado) 
+        VALUES($num1, $num2, $num3, $res)";
+
+if($conexao->query($sql)){
+    echo "<br>Dados salvos!com sucesso";
+}else{
+    echo "<br>Erro ao salvar dados!";
+}
+
+
+?>
